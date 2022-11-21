17ec138d11a67fec2ba8e78fd26a745d49e6e85e
anironL <115976133+anironL@users.noreply.github.com>
2022-11-21 09:46:37 -0500

    Update head.js

--- head.js
+++ head.js
@@ -1,19 +1,12 @@
-// FUNCTION IMPLEMENTATION
-const assertEqual = function(actual, expected) {
-    if (actual === expected){ 
-          console.log(`✔️ Assertion Passed: ${actual} === ${expected}`)
-      }
-      else {
-          console.log(`❌ Assertion Failed: ${actual} !== ${expected}`)
-      }
-  };
+const assertEqual = require('./assertEqual');
 
 const head = function(arrayToCheck){
     return arrayToCheck[0];
 }
 
-// TEST CODE
+/*TEST CODE
 assertEqual(head([5,6,7]), 5);
 assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
 assertEqual(head([1]), 1);
 assertEqual(head([]), "undefined");
+*/
