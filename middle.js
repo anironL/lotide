063b2f240758187d3731771d1b23c24b35e1db31
anironL <115976133+anironL@users.noreply.github.com>
2022-11-21 10:21:41 -0500

    Update middle.js

--- middle.js
+++ middle.js
@@ -1,48 +1,6 @@
-// FUNCTION IMPLEMENTATION
-const assertEqual = function(actual, expected) {
-  if (actual === expected){ 
-		console.log(`✔️ Assertion Passed: ${actual} === ${expected}`)
-	}
-	else {
-		console.log(`❌ Assertion Failed: ${actual} !== ${expected}`)
-	}
-};
-
-const eqArrays = function(actualArray, expectedArray) {
-  if (actualArray.length !== expectedArray.length){
-    console.log("false: differing array lengths");
-    return false;
-  }
-  else for (x = 0; x < actualArray.length; x++){
-    if (actualArray[x] === expectedArray[x]){
-//      console.log(`${actualArray[x]} === ${expectedArray[x]}`);
-    }
-    else{
-      console.log(`false: element ${x} of array 1 (${actualArray[x]}) !== array 2 (${expectedArray[x]})`);
-      return false;
-    }
-  }
-  console.log("true");
-  return true; 
-};
-
-const assertArraysEqual = function(actualArray, expectedArray) {
-  if (actualArray.length !== expectedArray.length){
-    console.log("false: differing array lengths");
-//    return false;
-  }
-  else for (x = 0; x < actualArray.length; x++){
-    if (actualArray[x] === expectedArray[x]){
-//      console.log(`${actualArray[x]} === ${expectedArray[x]}`);
-    }
-    else{
-      console.log(`false: element ${x} of array 1 (${actualArray[x]}) !== array 2 (${expectedArray[x]})`);
-//      return false;
-    }
-  }
-  console.log("true");
-//  return true; 
-};
+const assertEqual = require('./assertEqual');
+const eqArrays = require('./eqArrays');
+const assertArraysEqual = require('./assertArraysEqual');
 
 const middle = function(middleArray){
   console.log("length of array to check:", middleArray.length);
@@ -72,9 +30,4 @@ const middle = function(middleArray){
   }
 };
 
-// TEST CODE
-middle([1, 2, 3, 4]);
-middle([1]);
-middle([1, 2, 3]);
-middle([1,7,13,1,5,18,2,6,3]);
-middle([1,7,13,1,5,18,2,6,3,16]);
+module.exports = midde;
